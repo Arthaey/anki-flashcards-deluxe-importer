9243eb0799a346c110c9ef171fe9811f472da6d1
Arthaey <arthaey@gmail.com>
2015-08-01 11:53:15 -0500

    Use as a normal importer; remove menu item.

--- Flashcards_Deluxe_Importer.py
+++ Flashcards_Deluxe_Importer.py
@@ -22,15 +22,22 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 from anki.hooks import runHook
-from anki.importing import TextImporter
-from anki.importing.noteimp import NoteImporter, ForeignNote
 from aqt import mw
 from aqt.qt import *
 from aqt.utils import showInfo
 
-from flashcards_deluxe_importer.statistics import Statistics
+import anki.importing
+from anki.importing import TextImporter
+from anki.importing.anki1 import Anki1Importer
+from anki.importing.anki2 import Anki2Importer
+from anki.importing.apkg import AnkiPackageImporter
+from anki.importing.mnemo import MnemosyneImporter
+from anki.importing.noteimp import NoteImporter, ForeignNote
+from anki.importing.pauker import PaukerImporter
+from anki.importing.supermemo_xml import SupermemoXmlImporter
+from anki.lang import _
 
-fcdFilename = os.path.expanduser("~") + "/FCD-Miscellaneous.txt" # FIXME
+from flashcards_deluxe_importer.statistics import Statistics
 
 SECONDS_PER_DAY = 60*60*24
 
@@ -58,7 +65,6 @@ class FlashcardsDeluxeImporter(TextImporter):
         self.startedAt = datetime.now()
         self.newNoteIds = []
         self.clozeNoteIds = []
-        self.deckId = mw.col.decks.id("TEST")
 
     def importNotes(self, notes):
         NoteImporter.importNotes(self, notes)
@@ -229,14 +235,12 @@ class FlashcardsDeluxeImporter(TextImporter):
             runHook("leech", card)
         return suspendIds
 
-def importFlashcardsDeluxe():
-    # import into the collection (with whichever is the current deck)
-    importer = FlashcardsDeluxeImporter(mw.col, fcdFilename)
-    importer.initMapping()
-    importer.run()
-    showInfo("Finished importing {0} notes from {1}".format(
-        len(importer.newNoteIds), fcdFilename))
-
-action = QAction("Import from Flashcards Deluxe", mw)
-mw.connect(action, SIGNAL("triggered()"), importFlashcardsDeluxe)
-mw.form.menuTools.addAction(action)
+anki.importing.Importers = (
+    (_("Text separated by tabs or semicolons (*)"), TextImporter),
+    (_("Packaged Anki Deck (*.apkg *.zip)"), AnkiPackageImporter),
+    (_("Anki 1.2 Deck (*.anki)"), Anki1Importer),
+    (_("Mnemosyne 2.0 Deck (*.db)"), MnemosyneImporter),
+    (_("Supermemo XML export (*.xml)"), SupermemoXmlImporter),
+    (_("Pauker 1.8 Lesson (*.pau.gz)"), PaukerImporter),
+    (_("Flashcards Deluxe (*.txt)"), FlashcardsDeluxeImporter),
+)
